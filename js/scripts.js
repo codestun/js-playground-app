966926e89e16f42a668057e8295ba77e031ec37e
codestun <doerthy.k@gmail.com>
2023-05-08 12:14:50 +0200

    add array of  Pokémon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
-alert('Hello World!');
-
-let favFood = 'soup';
-document.write(favFood);
-
+// array of Pokémon objects
+let pokemonList = [
+  { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+  { name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
+  { name: 'Onix', height: 8.8, types: ['rock', 'ground'] },
+  { name: 'Shiftry', height: 1.3, types: ['dark', 'grass'] },
+  { name: 'Nidoking', height: 1.4, types: ['ground', 'poison'] },
+  { name: 'Zapdos', height: 1.6, types: ['electric', 'flying'] },
+  { name: 'Charizard', height: 1.7, types: ['fire', 'fighting'] },
+  { name: 'Squirtle', height: 1.8, types: ['water', 'fighting'] },
+  { name: 'Murkrow', height: 0.5, types: ['dark', 'flying'] }
+];
