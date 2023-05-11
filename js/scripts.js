b2999213f91f9a95d2b5ea5672b7fbf50a8922f1
codestun <doerthy.k@gmail.com>
2023-05-11 13:21:20 +0200

    change subject of objects to album, add new list

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,66 @@
-// array of Pokémon objects
-let pokemonList = [
-  { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
-  { name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
-  { name: 'Onix', height: 8.8, types: ['rock', 'ground'] },
-  { name: 'Shiftry', height: 1.3, types: ['dark', 'grass'] },
-  { name: 'Nidoking', height: 1.4, types: ['ground', 'poison'] },
-  { name: 'Zapdos', height: 1.6, types: ['electric', 'flying'] },
-  { name: 'Charizard', height: 1.7, types: ['fire', 'fighting'] },
-  { name: 'Squirtle', height: 1.8, types: ['water', 'fighting'] },
-  { name: 'Murkrow', height: 0.5, types: ['dark', 'flying'] }
+// Array of Albums taken from Spotify API
+let albums = [
+  {
+    name: "1989",
+    artist: "Taylor Swift",
+    album_type: "Pop",
+    release_date_precision: "2014-10-27",
+    tracks: ["Welcome to New York", "Blank Space", "Style", "Out of the Woods"]
+  },
+  {
+    name: "The Eminem Show",
+    artist: "Eminem",
+    album_type: "Hip-Hop",
+    release_date_precision: "2002-05-28",
+    tracks: ["White America", "Cleanin' Out My Closet", "Without Me", "Sing for the Moment"]
+  },
+  {
+    name: "Hybrid Theory",
+    artist: "Linkin Park",
+    album_type: "Alternative",
+    release_date_precision: "2000-10-24",
+    tracks: ["Papercut", "One Step Closer", "With You", "Crawling"]
+  },
+  {
+    name: "Bad",
+    artist: "Michael Jackson",
+    album_type: "Pop",
+    release_date_precision: "1987-08-31",
+    tracks: ["Bad", "The Way You Make Me Feel", "Man in the Mirror", "Smooth Criminal"]
+  },
+  {
+    name: "Between Two Shores",
+    artist: "Glen Hansard",
+    album_type: "Folk",
+    release_date_precision: "2018-01-19",
+    tracks: ["Roll On Slow", "Why Woman", "Wheels on Fire", "Wreckless Heart", "Movin' On", "Setting Forth", "Lucky Man", "One of Us Must Lose", "Your Heart's Not In It", "Time Will Be the Healer"]
+  },
+  {
+    name: "Thriller",
+    artist: "Michael Jackson",
+    album_type: "Pop",
+    release_date_precision: "1982-11-30",
+    tracks: ["Wanna Be Startin' Somethin'", "Thriller", "Beat It", "Billie Jean"]
+  },
+  {
+    name: "Rumours",
+    artist: "Fleetwood Mac",
+    album_type: "Rock",
+    release_date_precision: "1977-02-04",
+    tracks: ["Second Hand News", "Dreams", "Never Going Back Again", "Don't Stop"]
+  },
+  {
+    name: "Back in Black",
+    artist: "AC/DC",
+    album_type: "Rock",
+    release_date_precision: "1980-07-25",
+    tracks: ["Hells Bells", "Shoot to Thrill", "Back in Black", "You Shook Me All Night Long"]
+  },
+  {
+    name: "21",
+    artist: "Adele",
+    album_type: "Pop",
+    release_date_precision: "2011-01-24",
+    tracks: ["Rolling in the Deep", "Rumour Has It", "Turning Tables", "Someone Like You"]
+  }
 ];
